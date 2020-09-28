401ae0022d084c64bcf33a5acc3c2b62a3416fbb
STeven <steven.lignereux@hotmail.fr>
2020-09-28 22:34:18 +0200

    Index show create edit templates

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,4 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::resource('products', 'ProductController::class');
+Route::resource('products', ProductController::class);
